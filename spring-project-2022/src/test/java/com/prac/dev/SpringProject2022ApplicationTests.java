c686ac78f253c847899ad0ecba2d335d3425298a
sophiaxxx <sophia4619@gmail.com>
2022-10-25 17:10:03 +0800

    create first

---
+++ spring-project-2022/src/test/java/com/prac/dev/SpringProject2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prac.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
